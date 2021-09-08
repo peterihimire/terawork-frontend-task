80461893b8a25ecbde7926a0acb67ae7bd69a949
Peter Ihimire <peterihimire@gmail.com>
2021-09-08 17:02:03 +0100

    Upper part of the movieinfoblock seems to be 80% complete

--- src/components/MovieInfoBlock.js
+++ src/components/MovieInfoBlock.js
@@ -1,32 +1,46 @@
 import React from "react";
 import "./MovieInfoBlock.css";
-import { FaClock, FaStar, FaPlay } from "react-icons/fa";
+import movieImg from "../assets/img.png";
+import { FaClock, FaStar, FaPlay, FaHeart } from "react-icons/fa";
 
 const MovieInfoBlock = () => {
   return (
     <div className="movie-info-block">
       <div className="movie-info-block-main">
         <div className="movie-info-block-img">
-          <img src="" alt="movie poster" />
+          <img src={movieImg} alt="movie poster" />
         </div>
-        <div className="movie-info-text">
-          <h4>Mortal Kombat</h4>
-          <p>
-            This movie is for the lover of mortal Kombat. It is refreshing to
-            watchFind our taxis near you for affordable interstate transports
-          </p>
+        <div className="movie-info-block-text">
+          <div className="movie-info-block-detail">
+            <h4>Mortal Kombat</h4>
+            <p>
+              This movie is for the lover of mortal Kombat. It is refreshing to
+              watchFind our taxis near you for affordable interstate transports.
+              This movie is for the lover of mortal Kombat. It is refreshing to
+              watchFind our taxis near you for affordable interstate transports
+            </p>
+          </div>
+
           <div className="date-rate-time">
-            <div className="">
+            <div className="movie-info-block-icon">
               <FaClock className="movie-info-icon" />
               <p>23 April 2021</p>
             </div>
-            <div className="">
+            <div className="movie-info-block-icon">
               <FaStar className="movie-info-icon" />
               <p>6.2</p>
             </div>
-            <div className="">
+            <div className="movie-info-block-icon">
               <FaPlay className="movie-info-icon" />
               <p>1hr 50mins</p>
+            </div>
+          </div>
+          <div className="movie-info-block-btn-div">
+            <button className="btn search-btn movie-info-btn">watch now</button>
+            <div className="">
+              <div className=" movie-heart-div">
+                <FaHeart className="movie-info-icon movie-heart-icon" />
+              </div>
             </div>
           </div>
         </div>
